a32e95e48b71246a86465da8dc914c4abce5f6c4
Giovani O <giovani.o@outlook.com.br>
2021-10-05 14:39:12 -0300

    Spread operator e função mapState

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h2 class="font-weight-light">Contador: {{ $store.state.contador }}</h2>
+      <h2 class="font-weight-light">Contador: {{ contador }}</h2>
+      <h4 class="font-weight-light">Contador Alias: {{ contadorAlias }}</h4>
+      <h4 class="font-weight-light">Contador Multiplicado: {{ contadorMultiplicado }}</h4>
     <button 
       class="btn btn-lg btn-danger mr-1" 
       @click="decrementar"
@@ -13,7 +15,25 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
+  data() { 
+    return{ 
+      contadorLocal: 2
+    }
+  },
+  // computed: mapState(['contador']),
+  computed: {
+    ...mapState({ 
+      //contador: state => state.contador
+      contador: 'contador',
+      contadorAlias: 'contador',
+      contadorMultiplicado(state) {
+        return state.contador * this.contadorLocal
+      }
+    })
+  },
   methods: {
     incrementar() {
       this.$store.state.contador++
